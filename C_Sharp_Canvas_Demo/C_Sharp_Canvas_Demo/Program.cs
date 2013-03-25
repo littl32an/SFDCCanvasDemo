fa4112eb5a3e55ee9f6b6e7be4b9ce88064308ad
Anthony Do <tony.t.do@gmail.com>
2013-03-25 10:41:54 -0600

    connecting SQL SERVER 2008 R2

--- C_Sharp_Canvas_Demo/C_Sharp_Canvas_Demo/Program.cs
+++ C_Sharp_Canvas_Demo/C_Sharp_Canvas_Demo/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Collections.Generic;
+using System.Data.SqlClient;
 
 namespace C_Sharp_Canvas_Demo
 {
@@ -9,7 +11,25 @@ namespace C_Sharp_Canvas_Demo
     {
         static void Main(string[] args)
         {
+            DataClasses1DataContext db = new DataClasses1DataContext();
 
+            var g = from a in db.Table_2s select a;
+        }
+
+        static void showEmployees()
+        {
+            List<Employee> employees = new List<Employee>();
+        }
+    }
+
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Title { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("Name: {0}, Title: {1}", Name, Title);
         }
     }
 }
